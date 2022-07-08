6f041bee1f0f34e33919d9821270fbd6491cb3f9
kassem hachi <kassem.hachi@outlook.com>
2022-07-08 16:06:13 +0100

    Add button and Title to adsCard component

--- Components/AdsCard.js
+++ Components/AdsCard.js
@@ -1,9 +1,10 @@
 import React from 'react'
-
+import { Button } from '@mui/material'
 export default function AdsCard() {
     return (
-        <div className='col-spa n-1 from-blue-900 to-blue-700 bg-gradient-to-t rounded-md shadow-md  min-h-[400px]'>
-
+        <div className='col-span-1 from-blue-900 to-blue-700 bg-gradient-to-t rounded-md shadow-md  min-h-[400px] py-16 px-10'>
+            <Button className='bg-slate-200 rounded-full px-5 text-center'> NEW</Button>
+            <h1 className='text-white mt-4 text-2xl font-semibold'>We Have Added new invoicing Templates</h1>
         </div>
     )
 }
