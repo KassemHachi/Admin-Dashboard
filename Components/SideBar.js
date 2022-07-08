7ce584606c825f2e1c6a6e4c8b22ddc1f76dddf1
kassem hachi <kassem.hachi@outlook.com>
2022-07-08 14:47:56 +0100

    fixe topbar

--- Components/SideBar.js
+++ Components/SideBar.js
@@ -4,16 +4,16 @@ import ListItem from '../Components/ListItem';
 export default function SideBar() {
   return (
     <div className='col-span-1'>
-    <Logo/>
-    <div className='mt-10'>
-      <ul className='list-none '>
-      <ListItem href="#" Text="Home"/>
-      <ListItem href="#" Text="Invoices"/>
-      <ListItem href="#" Text="Clients"/>
-      <ListItem href="#" Text="Products"/>
-      <ListItem href="#" Text="Messages"/>
-      </ul>
+      <Logo />
+      <div className='mt-10'>
+        <ul className='list-none '>
+          <ListItem href="#" Text="Home" />
+          <ListItem href="#" Text="Invoices" />
+          <ListItem href="#" Text="Clients" />
+          <ListItem href="#" Text="Products" />
+          <ListItem href="#" Text="Messages" />
+        </ul>
+      </div>
     </div>
-  </div>
   )
 }
