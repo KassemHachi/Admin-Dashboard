61bfc76826ff1b7a3b33381144bf126c6c5eb4e5
kassem hachi <kassem.hachi@outlook.com>
2022-07-08 14:42:28 +0100

    Add UserImageSmall components

--- Components/Topbar.js
+++ Components/Topbar.js
@@ -2,6 +2,7 @@
 import { Input } from "@nextui-org/react";
 import { FaSearch } from 'react-icons/fa';
 import { RiNotificationLine } from 'react-icons/ri';
+import UserImageSmall from "./UserImageSmall";
 export default function Topbar() {
   return (
     <div className='grid grid-cols-4 '>
@@ -26,17 +27,8 @@ export default function Topbar() {
         <div className='flex gap-5'>
 
           <RiNotificationLine className='w-7 h-auto text-gray-400  hover:text-[#0d79f5] cursor-pointer hover:scale-125  transition-all ease-linear ' />
+          <UserImageSmall />
 
-          <div className=' flex items-center gap-3'>
-            <img
-              src="https://i.pinimg.com/564x/5a/5d/1d/5a5d1d0a05d050a89b7d836cf63ca35c.jpg"
-              className=' w-10 h-auto rounded-full overflow-clip cursor-pointer hover:scale-125 hover:shadow-sm transition-all ease-linear '
-            />
-            <div>
-              <h4 className='text-black font-semibold text-base'>Jahn Doe</h4>
-              <h6 className='text-gray-400 font-medium text-sm'>Developer</h6>
-            </div>
-          </div>
         </div>
       </div>
     </div>
---
+++ Components/UserImageSmall.js
@@ -0,0 +1,16 @@
+
+
+export default function UserImageSmall() {
+    return (
+        <div className=' flex items-center gap-3'>
+            <img
+                src="https://i.pinimg.com/564x/5a/5d/1d/5a5d1d0a05d050a89b7d836cf63ca35c.jpg"
+                className=' w-10 h-auto rounded-full overflow-clip cursor-pointer hover:scale-125 hover:shadow-sm transition-all ease-linear '
+            />
+            <div>
+                <h4 className='text-black font-semibold text-base'>Jahn Doe</h4>
+                <h6 className='text-gray-400 font-medium text-sm'>Developer</h6>
+            </div>
+        </div>
+    )
+}
