4760be610e6010fc6b8b56d5e8524a9d63f5b8fc
kassem hachi <kassem.hachi@outlook.com>
2022-07-11 13:47:45 +0100

    Make SummaryStatic responsive

--- Components/SummaryStatics.js
+++ Components/SummaryStatics.js
@@ -2,30 +2,30 @@ import React from 'react'
 import { BiLineChartDown, BiLineChart } from 'react-icons/bi';
 export default function SummaryStatics() {
   return (
-    <div className='mt-7 shadow-md grid grid-cols-4 rounded-xl bg-white w-full min-h-[100px] px-2 py-8'>
-      <div className='col-span-1 border-r-2 border-gray-200'>
+    <div className='mt-7 shadow-md grid grid-cols-2 lg:grid-cols-4  rounded-xl bg-white w-full min-h-[100px] px-2 py-8'>
+      <div className='col-span-1 my-4 lg:my-0  border-r-2 border-gray-200'>
         <div className='grid grid-cols-2'>
           <div className='col-span-1 flex justify-center items-center'>
             <BiLineChartDown className='text-5xl text-rose-700' />
           </div>
           <div className='col-span-1'>
             <h4>Total Revenue</h4>
-            <h1 className='text-2xl font-bold mt-1'>1,455 k</h1>
+            <h1 className='text-xl xl:text-2xl font-bold mt-1'>1,455 k</h1>
           </div>
         </div>
       </div>
-      <div className='col-span-1 border-r-2 border-gray-200'>
+      <div className='col-span-1 my-4 lg:my-0 border-r-0  lg:border-r-2 border-gray-200'>
         <div className='grid grid-cols-2'>
           <div className='col-span-1 flex justify-center items-center'>
             <BiLineChart className='text-5xl text-emerald-500' />
           </div>
           <div className='col-span-1'>
             <h4>Total Revenue</h4>
-            <h1 className='text-2xl font-bold mt-1'> 1,516</h1>
+            <h1 className='text-xl xl:text-2xl font-bold mt-1'> 1,516</h1>
           </div>
         </div>
       </div>
-      <div className='col-span-1 border-r-2 border-gray-200'>
+      <div className='col-span-1 my-4 lg:my-0  border-r-2 border-gray-200'>
         <div className='grid grid-cols-2'>
           <div className='col-span-1 flex justify-center items-center'>
             <BiLineChart className='text-5xl text-emerald-500' />
@@ -33,11 +33,11 @@ export default function SummaryStatics() {
           <div className='col-span-1'>
             <h4>Total Revenue</h4>
 
-            <h1 className='text-2xl font-bold mt-1'>  2,216</h1>
+            <h1 className='text-xl xl:text-2xl font-bold mt-1'>  2,216</h1>
           </div>
         </div>
       </div>
-      <div className='col-span-1'>
+      <div className='col-span-1 my-4 lg:my-0 '>
         <div className='grid grid-cols-2'>
           <div className='col-span-1 flex justify-center items-center'>
             <BiLineChartDown className='text-5xl text-rose-700' />
@@ -45,7 +45,7 @@ export default function SummaryStatics() {
           <div className='col-span-1'>
             <h4>Total Revenue</h4>
 
-            <h1 className='text-2xl font-bold mt-1'> $ 216k</h1>
+            <h1 className='text-xl xl:text-2xl font-bold mt-1'> $ 216k</h1>
           </div>
         </div>
       </div>
