903aa6e61fcb56aaa705819b6fe6e3ec4a83c0c4
kassem hachi <kassem.hachi@outlook.com>
2022-07-12 13:17:22 +0100

    Edit Table data

--- Components/IvoicesTabel.js
+++ Components/IvoicesTabel.js
@@ -3,35 +3,27 @@ import { DataGrid } from '@mui/x-data-grid';
 
 const columns = [
     { field: 'id', headerName: 'ID', width: 70 },
-    { field: 'firstName', headerName: 'First name', width: 130 },
-    { field: 'lastName', headerName: 'Last name', width: 130 },
+    { field: 'date', headerName: 'Date Created', width: 130 },
+    { field: 'clinet', headerName: 'Client', width: 130 },
     {
-        field: 'age',
-        headerName: 'Age',
+        field: 'amount',
+        headerName: 'Amount ($)',
         type: 'number',
         width: 90,
     },
-    {
-        field: 'fullName',
-        headerName: 'Full name',
-        description: 'This column has a value getter and is not sortable.',
-        sortable: false,
-        width: 160,
-        valueGetter: (params) =>
-            `${params.row.firstName || ''} ${params.row.lastName || ''}`,
-    },
+
 ];
 
 const rows = [
-    { id: 1, lastName: 'Snow', firstName: 'Jon', age: 35 },
-    { id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 42 },
-    { id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 45 },
-    { id: 4, lastName: 'Stark', firstName: 'Arya', age: 16 },
-    { id: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: null },
-    { id: 6, lastName: 'Melisandre', firstName: null, age: 150 },
-    { id: 7, lastName: 'Clifford', firstName: 'Ferrara', age: 44 },
-    { id: 8, lastName: 'Frances', firstName: 'Rossini', age: 36 },
-    { id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
+    { id: 1, date: '3 Jul, 2021', clinet: 'Jon', amount: 1522 },
+    { id: 2, date: '15 Feb, 2022', clinet: 'Cersei', amount: 7850 },
+    { id: 3, date: '27 Jan, 2020', clinet: 'Jaime', amount: 4530 },
+    { id: 4, date: '22 Mars, 2022', clinet: 'Arya', amount: 6580 },
+    { id: 5, date: '24 Nov, 2022', clinet: 'Daenerys', amount: 2500 },
+    { id: 6, date: '31 Oct, 2022', clinet: null, amount: 15000 },
+    { id: 7, date: '11 Jul, 2022', clinet: 'Ferrara', amount: 1570 },
+    { id: 8, date: '7 Dec, 2021', clinet: 'Rossini', amount: 740 },
+    { id: 9, date: '8 Oct, 2022', clinet: 'Harvey', amount: 650 },
 ];
 
 export default function IvoicesTabel(props) {
