d557b60acde9b1b30f9cd8425a072d5d7fbb297e
kassem hachi <kassem.hachi@outlook.com>
2022-07-12 15:54:22 +0100

    Add Animations classes to Tailwind.config.js file

--- Components/SideBar.js
+++ Components/SideBar.js
@@ -1,6 +1,10 @@
 import React from 'react'
 import Logo from '../Components/Logo';
 import ListItem from '../Components/ListItem';
+import { AiFillHome } from 'react-icons/ai';
+import { BsFillFileEarmarkRuledFill } from 'react-icons/bs';
+import { FaUserFriends, FaBox } from 'react-icons/fa';
+import { RiMessage3Fill } from 'react-icons/ri';
 export default function SideBar(props) {
 
 
@@ -8,12 +12,12 @@ export default function SideBar(props) {
     <div className='col-span-1'>
       <Logo />
       <div className='mt-14'>
-        <ul className='list-none '>
-          {props.ActivePage == 1 ? <ListItem isActive={true} href="/" Text="Home" /> : <ListItem isActive={false} href="/" Text="Home" />}
-          {props.ActivePage == 2 ? <ListItem isActive={true} href="/invoices" Text="Invoices" /> : <ListItem isActive={false} href="/invoices" Text="Invoices" />}
-          {props.ActivePage == 3 ? <ListItem isActive={true} href="#" Text="Clients" /> : <ListItem isActive={false} href="#" Text="Clients" />}
-          {props.ActivePage == 4 ? <ListItem isActive={true} href="#" Text="Products" /> : <ListItem isActive={false} href="#" Text="Products" />}
-          {props.ActivePage == 5 ? <ListItem isActive={true} href="#" Text="Messages" /> : <ListItem isActive={false} href="#" Text="Messages" />}
+        <ul className='list-none ml-5'>
+          {props.ActivePage == 1 ? <ListItem isActive={true} href="/" Text="Home" Icon={<AiFillHome className='text-2xl mr-4' />} /> : <ListItem isActive={false} href="/" Text="Home" Icon={<AiFillHome className='text-2xl mr-4' />} />}
+          {props.ActivePage == 2 ? <ListItem isActive={true} href="/invoices" Text="Invoices" Icon={<BsFillFileEarmarkRuledFill className='text-2xl mr-4' />} /> : <ListItem isActive={false} href="/invoices" Text="Invoices" Icon={<BsFillFileEarmarkRuledFill className='text-2xl mr-4' />} />}
+          {props.ActivePage == 3 ? <ListItem isActive={true} href="#" Text="Clients" Icon={<FaUserFriends className='text-2xl mr-4' />} /> : <ListItem isActive={false} href="#" Text="Clients" Icon={<FaUserFriends className='text-2xl mr-4' />} />}
+          {props.ActivePage == 4 ? <ListItem isActive={true} href="#" Text="Products" Icon={<FaBox className='text-2xl mr-4' />} /> : <ListItem isActive={false} href="#" Text="Products" Icon={<FaBox className='text-2xl mr-4' />} />}
+          {props.ActivePage == 5 ? <ListItem isActive={true} href="#" Text="Messages" Icon={<RiMessage3Fill className='text-2xl mr-4' />} /> : <ListItem isActive={false} href="#" Text="Messages" Icon={<RiMessage3Fill className='text-2xl mr-4' />} />}
         </ul>
       </div>
     </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,56 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        'fade-in-down': {
+          '0%': {
+            opacity: '0',
+            transform: 'translateY(-10px)'
+          },
+          '100%': {
+            opacity: '1',
+            transform: 'translateY(0)'
+          },
+        },
+        'fade-out-down': {
+          'from': {
+            opacity: '1',
+            transform: 'translateY(0px)'
+          },
+          'to': {
+            opacity: '0',
+            transform: 'translateY(10px)'
+          },
+        },
+        'fade-in-up': {
+          '0%': {
+            opacity: '0',
+            transform: 'translateY(20px)'
+          },
+          '100%': {
+            opacity: '1',
+            transform: 'translateY(0)'
+          },
+        },
+        'fade-out-up': {
+          'from': {
+            opacity: '1',
+            transform: 'translateY(0px)'
+          },
+          'to': {
+            opacity: '0',
+            transform: 'translateY(10px)'
+          },
+        }
+      },
+      animation: {
+        'fade-in-down': 'fade-in-down 1s ease-out',
+        'fade-out-down': 'fade-out-down 1s ease-out',
+        'fade-in-up': 'fade-in-up 1s ease-out',
+        'fade-out-up': 'fade-out-up 1s ease-out'
+      },
+    },
   },
   plugins: [],
 }
