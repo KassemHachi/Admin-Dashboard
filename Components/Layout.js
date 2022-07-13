75555cad69da448af2b947938bfbdf54c85162bf
kassem hachi <kassem.hachi@outlook.com>
2022-07-13 11:30:15 +0100

    Add Layout Component

---
+++ Components/Layout.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Layout({ children }) {
+    return (
+        <div className='grid grid-cols-4 pt-2  h-screen bg-gray-100 w-full fixed'>{children}</div>
+    )
+}
