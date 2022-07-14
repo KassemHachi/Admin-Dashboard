0970711a902c4eed1396c0d0a026a6752d1529d9
kassem hachi <kassem.hachi@outlook.com>
2022-07-14 12:27:08 +0100

    Add Topbar and heading to products page

--- pages/Products.js
+++ pages/Products.js
@@ -1,7 +1,23 @@
 import React from 'react'
+import Layout from '../Components/Layout'
+import Side from '../Components/Side'
+import SideBar from '../Components/SideBar'
+import Topbar from '../Components/Topbar'
 
 export default function Products() {
     return (
-        <div>Products</div>
+        <Layout >
+            <Side >
+                <SideBar ActivePage={4} />
+            </Side>
+
+            <main className='col-span-3 p-4  overflow-y-scroll'>
+
+                <Topbar />
+                <div className='animate-fade-in-up flex items-center justify-between  px-3 mt-10'>
+                    <h1 className='text-lg md:text-2xl font-semibold w-fit'>Products list:</h1>
+                </div>
+            </main>
+        </Layout >
     )
 }
