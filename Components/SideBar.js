e44852bb546a50184a680de0532626e9dd350439
kassem hachi <kassem.hachi@outlook.com>
2022-07-14 12:47:39 +0100

    add redirect link to 404 page

--- Components/SideBar.js
+++ Components/SideBar.js
@@ -15,9 +15,9 @@ export default function SideBar(props) {
         <ul className='list-none ml-0 md:ml-5'>
           {props.ActivePage == 1 ? <ListItem isActive={true} href="/" Text="Home" Icon={<AiFillHome className='text-2xl mr-0 md:mr-4' />} /> : <ListItem isActive={false} href="/" Text="Home" Icon={<AiFillHome className='text-2xl mr-0 md:mr-4' />} />}
           {props.ActivePage == 2 ? <ListItem isActive={true} href="/invoices" Text="Invoices" Icon={<BsFillFileEarmarkRuledFill className='text-2xl mr-0 md:mr-4' />} /> : <ListItem isActive={false} href="/invoices" Text="Invoices" Icon={<BsFillFileEarmarkRuledFill className='text-2xl mr-0 md:mr-4' />} />}
-          {props.ActivePage == 3 ? <ListItem isActive={true} href="#" Text="Clients" Icon={<FaUserFriends className='text-2xl mr-0 md:mr-4' />} /> : <ListItem isActive={false} href="#" Text="Clients" Icon={<FaUserFriends className='text-2xl mr-0 md:mr-4' />} />}
+          {props.ActivePage == 3 ? <ListItem isActive={true} href="/clients" Text="Clients" Icon={<FaUserFriends className='text-2xl mr-0 md:mr-4' />} /> : <ListItem isActive={false} href="/clients" Text="Clients" Icon={<FaUserFriends className='text-2xl mr-0 md:mr-4' />} />}
           {props.ActivePage == 4 ? <ListItem isActive={true} href="/products" Text="Products" Icon={<FaBox className='text-2xl mr-0 md:mr-4' />} /> : <ListItem isActive={false} href="/products" Text="Products" Icon={<FaBox className='text-2xl mr-0 md:mr-4' />} />}
-          {props.ActivePage == 5 ? <ListItem isActive={true} href="#" Text="Messages" Icon={<RiMessage3Fill className='text-2xl mr-0 md:mr-4' />} /> : <ListItem isActive={false} href="#" Text="Messages" Icon={<RiMessage3Fill className='text-2xl mr-0 md:mr-4' />} />}
+          {props.ActivePage == 5 ? <ListItem isActive={true} href="/messages" Text="Messages" Icon={<RiMessage3Fill className='text-2xl mr-0 md:mr-4' />} /> : <ListItem isActive={false} href="/messages" Text="Messages" Icon={<RiMessage3Fill className='text-2xl mr-0 md:mr-4' />} />}
         </ul>
       </div>
     </>
--- pages/404.js
+++ pages/404.js
@@ -1,9 +1,10 @@
 import React from 'react'
-import { Link } from 'react-admin'
+import Link from 'next/link';
 import Layout from '../Components/Layout'
 import Side from '../Components/Side'
 import SideBar from '../Components/SideBar'
 import Topbar from '../Components/Topbar'
+import { Modal, Button, Image, Text, Link as LINK } from "@nextui-org/react";
 
 export default function Page404() {
     return (
@@ -18,7 +19,11 @@ export default function Page404() {
                 <div className='flex items-center justify-center  h-[70vh]'>
                     <div className='animate-fade-in-up   px-3 mt-10 '>
                         <h1 className='text-2xl md:text-4xl my-2 font-bold w-fit'>ERROR 404 :</h1>
-                        <h2 className='text-xl md:text-2xl font-semibold w-fit'>Page not found return to <Link src="/">home</Link> </h2>
+                        <h2 className='text-xl md:text-2xl font-semibold w-fit'>Page not found return to{" "}
+                            <Link href={"/"}>
+                                <LINK className='cursor-pointer'>home</LINK>
+                            </Link>
+                        </h2>
                     </div>
                 </div>
             </main>
