636b5b5ddf8ad86ae84a5078b9e5dffdb77c8342
kassem hachi <kassem.hachi@outlook.com>
2022-07-14 11:56:00 +0100

    edit User name and Description in Different Screens

--- Components/UserImageSmall.js
+++ Components/UserImageSmall.js
@@ -2,12 +2,12 @@
 
 export default function UserImageSmall() {
     return (
-        <div className=' flex items-center gap-3'>
+        <div className=' flex items-center gap-0  md:gap-3'>
             <img
                 src="https://i.pinimg.com/564x/5a/5d/1d/5a5d1d0a05d050a89b7d836cf63ca35c.jpg"
                 className=' w-10 h-auto rounded-full overflow-clip cursor-pointer hover:scale-125 hover:shadow-sm transition-all ease-linear '
             />
-            <div>
+            <div className="hidden md:inline">
                 <h4 className='text-black font-semibold text-sm md:text-base'>Jahn Doe</h4>
                 <h6 className='text-gray-400 font-medium text-xs md:text-sm'>Developer</h6>
             </div>
