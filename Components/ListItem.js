4704e6d40455ca38de16b654f0c97a439ee2f284
kassem hachi <kassem.hachi@outlook.com>
2022-07-14 11:48:58 +0100

    Edit Sidebar icons in md and lg screens

--- Components/ListItem.js
+++ Components/ListItem.js
@@ -8,9 +8,9 @@ export default function ListItem(props) {
 
 
   return (
-    <li className='mx-1 md:mx-8 my-5 text-lg lg:text-xl items-start cursor-pointer'>
+    <li className='ml-3 lg:mx-8 my-5 text-md lg:text-xl items-start cursor-pointer'>
       <Link href={props.href}  >
-        <LINK style={props.isActive == true ? { backgroundColor: '#4e80ed', color: "#fff" } : {}} className={props.isActive == true ? " rounded-lg  text-center  px-5 py-3  w-full  transition-all ease-in duration-500" : "rounded-lg  text-center text-blue-600 bg-transparent px-2 py-3   hover:bg-blue-600  w-full hover:text-white  transition-all ease-in duration-500"}>
+        <LINK style={props.isActive == true ? { backgroundColor: '#4e80ed', color: "#fff" } : {}} className={props.isActive == true ? " rounded-lg  text-center px-1 lg:px-5 py-3  w-full  transition-all ease-in duration-500" : "rounded-lg  text-center text-blue-600 bg-transparent px-0 lg:px-2 py-3   hover:bg-blue-600  w-full hover:text-white  transition-all ease-in duration-500"}>
           <div className='flex items-center justify-center w-full'>  {props.Icon}
             <p className='hidden md:inline'>{props.Text}</p></div>
         </LINK>
