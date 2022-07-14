29d36cce0def687b524bfe2417a7bc0ad87baebb
kassem hachi <kassem.hachi@outlook.com>
2022-07-14 13:33:01 +0100

    Add to progress bar on load page

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,18 @@
 import '../styles/globals.css'
+import { Router } from "next/router";
+import Progress from "@aldabil/next-progress";
 
+//Progress setup
+Progress.configure({
+  type: "bar",
+  background:
+    "linear-gradient(90deg, rgb(78,128,237) 0%, rgb(0,114,245) 100%, rgba(127,137,0,1) 100%)",
+  height: 3,
+  //svg: "used with type='fullpage' ",
+});
+Router.events.on("routeChangeStart", () => Progress.start());
+Router.events.on("routeChangeComplete", () => Progress.complete());
+Router.events.on("routeChangeError", () => Progress.complete());
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
