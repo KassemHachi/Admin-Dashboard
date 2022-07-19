6c8029152ddadf7d1f44da1a6e7a04795051ee65
kassem hachi <kassem.hachi@outlook.com>
2022-07-19 15:18:45 +0100

    Resize modal width

--- pages/index.js
+++ pages/index.js
@@ -14,26 +14,24 @@ import Main from '../Components/Main';
 export default function Home() {
 
   return (
-    <>
-      <Head>
-        <title>Admin Dashboard</title>
-      </Head>
-      <Layout>
-        <Side >
-          <SideBar ActivePage={1} />
-        </Side>
-        <Main className='col-span-3 p-4 lg:pr-12  overflow-y-scroll'>
 
-          <Topbar />
-          <SummaryStatics />
 
-          <div className='grid grid-cols-1  md:grid-cols-2 lg:grid-cols-3 mt-5 gap-7'>
-            <StaticsCard className='col-span-1 lg:col-span-2 bg-white rounded-md shadow-md  sm:h-fit md:h-full ' />
-            <AdsCard className='col-span-1 flex-col justify-center text-center from-blue-900 to-blue-700 bg-gradient-to-t rounded-md shadow-md sm:h-fit md:h-full py-16 px-10' />
-          </div>
-        </Main>
+    <Layout>
+      <Side >
+        <SideBar ActivePage={1} />
+      </Side>
+      <Main className='col-span-3 p-4 lg:pr-12  overflow-y-scroll'>
 
-      </Layout>
-    </>
+        <Topbar />
+        <SummaryStatics />
+
+        <div className='grid grid-cols-1  md:grid-cols-2 lg:grid-cols-3 mt-5 gap-7'>
+          <StaticsCard className='col-span-1 lg:col-span-2 bg-white rounded-md shadow-md  sm:h-fit md:h-full ' />
+          <AdsCard className='col-span-1 flex-col justify-center text-center from-blue-900 to-blue-700 bg-gradient-to-t rounded-md shadow-md sm:h-fit md:h-full py-16 px-10' />
+        </div>
+      </Main>
+
+    </Layout>
+
   )
 }
