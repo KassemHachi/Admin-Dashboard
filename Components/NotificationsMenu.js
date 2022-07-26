7056029421f0d069960ca81472f70212e02460fc
KassemHachi <kassemhachi@gmail.com>
2022-07-26 19:47:19 +0100

    Add Tooltip date to notification

--- Components/NotificationsMenu.js
+++ Components/NotificationsMenu.js
@@ -1,5 +1,5 @@
 import { Badge } from '@mui/material'
-import { Avatar, Dropdown } from '@nextui-org/react'
+import { Avatar, Dropdown,Tooltip } from '@nextui-org/react'
 import React from 'react'
 import { RiNotification4Fill } from 'react-icons/ri'
 
@@ -10,12 +10,12 @@ export default function NotificationsMenu() {
             <RiNotification4Fill className=' w-7 h-auto text-gray-400  group-hover:text-[#0d79f5] cursor-pointer hover:scale-110  transition-all ease-linear ' />
          </Dropdown.Button> </Badge>
       <Dropdown.Menu aria-label="Dynamic Actions" className="w-16" css={{ $$dropdownMenuWidth: "300px" }}>
- 
-      <Dropdown.Item className="py-8 mt-1"
+     
+ <Dropdown.Item className="py-8 mt-1"
             key={Math.floor(Math.random() * (1000 - 1 + 1) + 1)}
             color={"default"}
             
-          >
+          > <Tooltip content="09:03 20-06-2022" placement="left" arrow>
           <div className="flex items-center">
               <Avatar
           src="https://i.pravatar.cc/150?u=a042581f4e29026024d"
@@ -25,12 +25,16 @@ export default function NotificationsMenu() {
         Liked your photo
           </div>
       
+</Tooltip>
           </Dropdown.Item>
-          <Dropdown.Item className="py-8 mt-1"
+
+        
+
+   <Dropdown.Item className="py-8 mt-1"
             key={Math.floor(Math.random() * (1000 - 1 + 1) + 1)}
             color={"default"}
             
-          >
+          ><Tooltip  content="19:33 25-07-2022" placement="left" arrow>
           <div className="flex items-center">
               <Avatar
           src="https://i.pravatar.cc/150?u=a042581f4e29026024d"
@@ -39,9 +43,11 @@ export default function NotificationsMenu() {
         />
         Liked your photo
           </div>
-      
+          </Tooltip>
+         
           </Dropdown.Item>
-          
+
+
 
     
 
